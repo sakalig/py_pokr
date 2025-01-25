8f9245973bcbc6f5709f1ec33a47c478c71b9dd5
Gabriel Sakali <gabedevs@gmail.com>
2025-01-25 21:16:31 +0300

    Add: modularization - main function

--- main.py
+++ main.py
@@ -2,18 +2,22 @@ import sys
 import requests
 import json
 
-if len(sys.argv) == 2:
-    # TODO: Process arguments to be viable search string
-    # Proceed to append
-    print("Searching artists' songs...")
-    response = requests.get("https://itunes.apple.com/search?entity=song&limit=8&term=" + sys.argv[1])
-    j = response.json()
-    # For dev consumption
-    print(json.dumps(j, indent=2))
+def main():
+    if len(sys.argv) == 2:
+        # TODO: Process arguments to be viable search string
+        # Proceed to append
+        print("Searching artists' songs...")
+        response = requests.get("https://itunes.apple.com/search?entity=song&limit=8&term=" + sys.argv[1])
+        j = response.json()
+        # For dev consumption
+        print(json.dumps(j, indent=2))
 
-    for result in j["results"]:
-        print(result["trackName"])
-elif len(sys.argv) > 2:
-    print("Kindly pass multiple arguments within quotes")
-else:
-    pass
+        for result in j["results"]:
+            print(result["trackName"])
+    elif len(sys.argv) > 2:
+        print("Kindly pass multiple arguments within quotes")
+    else:
+        pass
+
+if __name__ == "__main__":
+    main()
