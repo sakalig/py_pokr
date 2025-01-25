d474f92d778e7bb4b4d42a3871a2280da3ac8752
Gabriel Sakali <gabedevs@gmail.com>
2025-01-25 21:08:07 +0300

    Add: user friendly output

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import json
 if len(sys.argv) == 2:
     # TODO: Process arguments to be viable search string
     # Proceed to append
+    print("Searching artists' songs...")
     response = requests.get("https://itunes.apple.com/search?entity=song&limit=8&term=" + sys.argv[1])
     j = response.json()
     # For dev consumption
